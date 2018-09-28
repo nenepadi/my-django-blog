2bdfd9d3172f67c18a929f462b733192eda8a3fa
Obed Ademang <obed.ademang@gmail.com>
2018-09-28 08:16:36 +0000

    Suppress error thrown when publishing a post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class Post(models.Model):
@@ -10,7 +11,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = datetime.datetime.now()
         self.save()
 
     def __str__(self):
